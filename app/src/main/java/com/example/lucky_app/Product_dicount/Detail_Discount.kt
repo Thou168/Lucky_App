9f5d6336af284d21f5fca22bbb05c4b396614715
pongm <mengthean28@gmail.com>
2019-07-04 13:19:46 +0700

    map

--- app/src/main/java/com/example/lucky_app/Product_dicount/Detail_Discount.kt
+++ app/src/main/java/com/example/lucky_app/Product_dicount/Detail_Discount.kt
@@ -5,116 +5,107 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.*
-import android.location.LocationListener
-import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.StrikethroughSpan
 import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
 import com.custom.sliderimage.logic.SliderImage
 import com.example.lucky_app.R
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
 
 import de.hdodenhof.circleimageview.CircleImageView
 import java.io.IOException
 import java.util.*
 
 class Detail_Discount : AppCompatActivity(), OnMapReadyCallback{
-    internal lateinit var locationManager: LocationManager
-    internal lateinit var txt_place: TextView
-    private val REQUEST_LOCATION = 1
+    private val TAG = Detail_Discount::class.java!!.getSimpleName()
+    private lateinit var mMap: GoogleMap
 
     private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
     private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     private var mLocationPermissionGranted: Boolean = false
     private var mLastKnownLocation: Location? = null
-    internal var locationListener: LocationListener? = null
-    private lateinit var mGoogleMap: GoogleMap
 
-    override fun onMapReady(googleMap: GoogleMap) {
+    internal lateinit var txt_place: TextView
+    private val REQUEST_LOCATION = 1
+    internal lateinit var locationManager: LocationManager
+    internal lateinit var locationListener: LocationListener
+
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
         locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        mGoogleMap = googleMap
 
         locationListener = object : LocationListener {
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+            }
+
             override fun onLocationChanged(location: Location) {
-
-            }
-
-            override fun onStatusChanged(s: String, i: Int, bundle: Bundle) {
-
-            }
-
-            override fun onProviderEnabled(s: String) {
-
-            }
-
-            override fun onProviderDisabled(s: String) {
-
-            }
-        }
-        if (ActivityCompat.checkSelfPermission(this@Detail_Discount, Manifest.permission.ACCESS_FINE_LOCATION) !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this@Detail_Discount, Manifest.permission.ACCESS_COARSE_LOCATION) !== PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this@Detail_Discount, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
-        } else {
-            val location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            if (location != null) {
                 val geocoder = Geocoder(applicationContext, Locale.getDefault())
                 try {
-                    val listAddress = geocoder.getFromLocation(location.latitude, location.longitude, 3)
-                    txt_place.text = txt_place.text.toString() + "" + listAddress[0].subThoroughfare
-                    txt_place.text = txt_place.text.toString() + "," + listAddress[0].adminArea
-                    txt_place.text = txt_place.text.toString() + "," + listAddress[0].countryCode
-
+                    val listAddress = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                    //txt_place.text = txt_place.text.toString() + "" + listAddress[0].getAddressLine(0)
+                    txt_place.text = listAddress[0].featureName + ", " + listAddress[0].adminArea + ", " + listAddress[0].countryName
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
-
-            } else {
-                Toast.makeText(this, "Unble to trace your location", Toast.LENGTH_SHORT).show()
             }
         }
+
+        if (ActivityCompat.checkSelfPermission(this@Detail_Discount, Manifest.permission.ACCESS_FINE_LOCATION) !== PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this@Detail_Discount, Manifest.permission.ACCESS_COARSE_LOCATION) !== PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@Detail_Discount, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
+        } else {
+
+        }
+
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
         val lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//        mGoogleMap.clear()
+        mMap.clear()
         val userLocation = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
-        mGoogleMap.addMarker(MarkerOptions().position(userLocation).title("My location"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
-        mGoogleMap.animateCamera(CameraUpdateFactory.zoomBy(12f))
-        mGoogleMap.moveCamera(CameraUpdateFactory.zoomBy(15f))
-        mGoogleMap.isMyLocationEnabled = true
+        mMap.addMarker(MarkerOptions().position(userLocation).title("Do you know? I`m here"))
+        p0.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(12f))
+        mMap.moveCamera(CameraUpdateFactory.zoomBy(15f))
+        mMap.isMyLocationEnabled = true
         //disable
-        mGoogleMap.uiSettings.isScrollGesturesEnabled = false
+        mMap.uiSettings.isScrollGesturesEnabled = false
         //mMap.getUiSettings().setZoomGesturesEnabled(false);
         getLocationPermission()
 
         getDeviceLocation()
-        }
-
+    }
     private fun getDeviceLocation() {
         try {
             if (mLocationPermissionGranted) {
-                val locationResult = mFusedLocationProviderClient!!.getLastLocation()
+                val locationResult = mFusedLocationProviderClient!!.lastLocation
                 locationResult.addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         mLastKnownLocation = task.result
-                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(mLastKnownLocation!!.getLatitude(), mLastKnownLocation!!.getLongitude()), 15f))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(mLastKnownLocation!!.latitude, mLastKnownLocation!!.longitude), 15f))
                     } else {
-                        mGoogleMap.uiSettings.isMyLocationButtonEnabled = false
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
                     }
                 }
             }
@@ -125,26 +116,26 @@ class Detail_Discount : AppCompatActivity(), OnMapReadyCallback{
     }
     private fun getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.applicationContext,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION) === PackageManager.PERMISSION_GRANTED) {
+                        Manifest.permission.ACCESS_FINE_LOCATION) === PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true
         } else {
             ActivityCompat.requestPermissions(this,
-                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
         }
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_discount)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
 
+        txt_place = findViewById(R.id.txt_show_place) as TextView
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
-        txt_place = findViewById(R.id.txt_show_place) as TextView
-
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         findViewById<TextView>(R.id.tv_back).setOnClickListener { finish() }
 
