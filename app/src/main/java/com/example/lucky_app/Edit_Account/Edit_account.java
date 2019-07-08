dfe3b6faed0cf303abfa4da5750df2020da1a58b
borithnget <34413051+borithnget@users.noreply.github.com>
2019-07-08 12:53:15 +0700

    Update for take photo on post

--- app/src/main/java/com/example/lucky_app/Edit_Account/Edit_account.java
+++ app/src/main/java/com/example/lucky_app/Edit_Account/Edit_account.java
@@ -49,6 +49,7 @@ import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class Edit_account extends AppCompatActivity {
@@ -57,7 +58,6 @@ public class Edit_account extends AppCompatActivity {
     private static final String TAG = "Response";
     private LinearLayout layout_public_user,layout_121_dealer;
     private TextView tvType_121,tvBack;
-    private EditText etShopName,etShopAddr,etResponsible,etWingNumber_121,etWingName_121,etPhone_121;
     //private TextView tvType,genderSpinner,pobSpinner,locationSpinner,tvAddress,maritalStatusSpinner;
     private EditText etUsername,etDob,etJob,etWingNumber,etWingName,etPhone;
     private TextInputLayout tilusername,tildob,tiljob,tilwingnumber,tilwingname,tilphone,tilShopName,tilshopAddr,tilresponsible;
@@ -78,18 +78,14 @@ public class Edit_account extends AppCompatActivity {
     private EditText date;
     int mMonth,mYear,mDay;
 
+    private TextView tvUserGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account);
         TextView back = (TextView) findViewById(R.id.tv_Back);
 
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
 //        layout_public_user = (LinearLayout)findViewById(R.id.layout_type_public_user);
 //        layout_121_dealer  = (LinearLayout)findViewById(R.id.layout_type_121_dealer);
@@ -97,23 +93,23 @@ public class Edit_account extends AppCompatActivity {
         prefer = getSharedPreferences("Register",MODE_PRIVATE);
         if (prefer.contains("token")) {
             pk = prefer.getInt("Pk",0);
-            user_id = String.valueOf(pk);
-            Log.d(TAG, user_id);
         }else if (prefer.contains("id")) {
-            id = prefer.getInt("id", 0);
-            user_id = String.valueOf(id);
-            Log.d(TAG, user_id);
+            pk = prefer.getInt("id", 0);
         }
-        final String url = String.format("%s%s%s/", ConsumeAPI.BASE_URL,"api/v1/users/",user_id);
+        final String url = String.format("%s%s%s/", ConsumeAPI.BASE_URL,"api/v1/users/",pk);
         name = prefer.getString("name","");
         pass = prefer.getString("pass","");
-        Encode = getEncodedString(name,pass);
+        Encode =getEncodedString(name,pass);
+        Log.e(TAG,name+" "+pass+" "+pk+" "+Encode+" "+url);
         initialUserInformation(url,Encode);
 
-//        ID_Field();
-        //  Groups(url,Encode);
-//        DropList();
-//Dropdown
+        tvUserGroup=(TextView) findViewById(R.id.tvUserGroup);
+        etUsername=(EditText) findViewById(R.id.Username);
+        etWingName=(EditText) findViewById(R.id.wing_account);
+        etWingNumber=(EditText) findViewById(R.id.wing_number);
+        //etPhone=(EditText) findViewById(R.id.phone);
+        etJob=(EditText) findViewById(R.id.job);
+
         gender = (Spinner) findViewById(R.id.gender);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -130,7 +126,7 @@ public class Edit_account extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.location, android.R.layout.simple_spinner_item);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         location.setAdapter(adapter3);
-//Date
+        //Date
         date = (EditText) findViewById(R.id.birth);
         date.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -138,7 +134,22 @@ public class Edit_account extends AppCompatActivity {
                 showDatePickerDialog();
             }
         });
-    } //create
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button btSubmit=(Button) findViewById(R.id.btn_EditAccount);
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //summitUserInformation();
+                PutData(url,Encode);
+            }
+        });
+    }
     private void showDatePickerDialog(){
         final Calendar c = Calendar.getInstance();
         mYear = c.get(Calendar.YEAR);
@@ -198,104 +209,35 @@ public class Edit_account extends AppCompatActivity {
                 String mMessage = response.body().string();
                 Log.e(TAG,mMessage);
                 Gson gson = new Gson();
-                User convertJsonJava = new User();
+
                 try{
-                    convertJsonJava = gson.fromJson(mMessage,User.class);
-                    int[] gg = convertJsonJava.getGroups();
-                    g = gg[0];
-                    if (g==1){
-                        //groupsSpinner.setSelection(0);
 
-                    }else if (g==2){
-                        //tvType.setText("121 Dealer");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            User convertJsonJava = new User();
+                            convertJsonJava = gson.fromJson(mMessage,User.class);
+                            int[] gg = convertJsonJava.getGroups();
+                            etUsername.setText(convertJsonJava.getUsername());
+        //                    tvGender.setText(convertJsonJava.getGender());
+        //                    etDob   .setText(convertJsonJava.getData_of_birth());
+        //                    tvPob.setText(convertJsonJava.getPlace_of_birth());
+        //                    tvMarried.setText(convertJsonJava.getMarital_status());
+                            if(convertJsonJava.getProfile()!=null) {
+                                etWingNumber.setText(convertJsonJava.getProfile().getWing_account_number());
+                                etWingName.setText(convertJsonJava.getProfile().getWing_account_name());
+                                etJob.setText(convertJsonJava.getProfile().getJob());
+                            }
+                        }
+                    });
 
-                    }else if (g==3){
-                        //tvType.setText("Dealer");
 
-                    }
-//                    etUsername.setText(convertJsonJava.getUsername());
-//                    tvGender.setText(convertJsonJava.getGender());
-//                    etDob   .setText(convertJsonJava.getData_of_birth());
-//                    tvPob.setText(convertJsonJava.getPlace_of_birth());
-//                    tvMarried.setText(convertJsonJava.getMarital_status());
-//                    etWingNumber.setText(convertJsonJava.getWing_account_number());
-//                    etWingName.setText(convertJsonJava.getWing_account_name());
-//                    etJob.setText(convertJsonJava.getJob());
-//
-//                    etShopAddr.setText(convertJsonJava.getShop_address());
-//                    etShopName.setText(convertJsonJava.getShop_name());
-//                    etResponsible.setText(convertJsonJava.getResponsible_officer());
-//                    etWingNumber_121.setText(convertJsonJava.getWing_account_number());
-//                    etWingName_121.setText(convertJsonJava.getWing_account_name());
                 }catch (JsonParseException e){
                     e.printStackTrace();
                 }
             }
         });
     }
-    @SuppressWarnings("unchecked")
-//    private void ID_Field() {
-//        // public user textview
-//        spinner     =(MaterialSpinner) layout_public_user.findViewById(R.id.tvType);
-//        tvGender   = (TextView)layout_public_user.findViewById(R.id.tvGender);
-//        tvPob      =(TextView) layout_public_user.findViewById(R.id.tvPob);
-//        tvLocation = (TextView)layout_public_user.findViewById(R.id.tvLocation);
-//        tvMarried  = (TextView)layout_public_user.findViewById(R.id.tvMarried);
-////public user edittext
-//        etUsername  = (EditText)layout_public_user.findViewById(R.id.etUsername);
-//        etDob       = (EditText)layout_public_user.findViewById(R.id.ed_date_of_birth );
-//        etJob       = (EditText)layout_public_user.findViewById(R.id.edJob );
-//        etWingNumber= (EditText)layout_public_user.findViewById(R.id.etWingNumber );
-//        etWingName  = (EditText)layout_public_user.findViewById(R.id.etWingName );
-//        etPhone     = (EditText)layout_public_user.findViewById(R.id.etAccount_Phone );
-//        etShopName  = (EditText)layout_public_user.findViewById(R.id.etShop_Name);
-//        etShopAddr  = (EditText)layout_public_user.findViewById(R.id. etShop_Addr);
-//        etResponsible= (EditText)layout_public_user.findViewById(R.id.etResponsible );
-//
-//        // 121 dealer TextView
-//        tvType_121 = (TextView) layout_121_dealer.findViewById(R.id.tvType);
-//        // 121 dealer EditText
-//        etShopName       =(EditText)layout_121_dealer.findViewById(R.id.etShop_Name);
-//        etShopAddr       =(EditText)layout_121_dealer.findViewById(R.id.etShop_Addr );
-//        etWingName_121   =(EditText)layout_121_dealer.findViewById(R.id.etWingName );
-//        etWingNumber_121 =(EditText)layout_121_dealer.findViewById(R.id.etWingNumber );
-//        etResponsible    =(EditText)layout_121_dealer.findViewById(R.id.etResponsible );
-//        etPhone_121      =(EditText)layout_121_dealer.findViewById(R.id.etAccount_Phone );
-//        btnsubmit = (Button)findViewById(R.id.btn_EditAccount);
-//        tvBack = (TextView)findViewById(R.id.tvBack_account);
-//        tvBack.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-//
-//textinputlayout
-//     tilusername= (TextInputLayout)findViewById(R.id.tilUsername );
-//     tildob     = (TextInputLayout)findViewById(R.id.tilDob );
-//     tiljob     = (TextInputLayout)findViewById(R.id.tilJob );
-//     tilwingnumber= (TextInputLayout)findViewById(R.id.tilWingNumber );
-//     tilwingname= (TextInputLayout)findViewById(R.id.tilWingName );
-//     tilphone   = (TextInputLayout)findViewById(R.id.tilAccount_phone );
-//     tilShopName= (TextInputLayout)findViewById(R.id.tilShop_Name);
-//     tilshopAddr= (TextInputLayout)findViewById(R.id.tilShop_Addr );
-//     tilresponsible= (TextInputLayout)findViewById(R.id.tilResponsible );
-//
-//       imgType            = (ImageView)findViewById(R.id.imgType );
-//       imgGender          = (ImageView)findViewById(R.id.imgGender );
-//       imgPob             = (ImageView)findViewById(R.id.imgPob );
-//       imgLocation        = (ImageView)findViewById(R.id.imgLocation );
-//       imgAddress         = (ImageView)findViewById(R.id.imgAccount_Address );
-//       imgMarried         = (ImageView)findViewById(R.id.imgMarried );
-//       imgtilUsername     = (ImageView)findViewById(R.id.imgUsername);
-//       imgtilDob          = (ImageView)findViewById(R.id.imgDob );
-//       imgtilWingNumber   = (ImageView)findViewById(R.id.imgWingNumber );
-//       imgtilWingName     = (ImageView)findViewById(R.id.imgWingName );
-//       imgtilPhone        = (ImageView)findViewById(R.id.imgAccount_Phone );
-//       imgtilShopName     = (ImageView)findViewById(R.id.imgShop_name);
-//       imgtilShopAddr     = (ImageView)findViewById(R.id.imgShop_Addr);
-//       imgtilResponsible  = (ImageView)findViewById(R.id.imgResponsible );
-//    }
 
     private void InitialProvinceDatas(){
         String API_ENDPOINT=ConsumeAPI.BASE_URL+"api/v1/provinces/";
@@ -328,5 +270,130 @@ public class Edit_account extends AppCompatActivity {
         });
         mQueue.add(request);
     }
+    private void summitUserInformation(){
+        String URL_ENDPOINT=ConsumeAPI.BASE_URL+"api/v1/users/"+pk;
+        OkHttpClient client=new OkHttpClient();
+        JSONObject user=new JSONObject();
+        JSONObject profile=new JSONObject();
+        JSONObject profile_detail=new JSONObject();
+        try{
 
+            profile_detail.put("gender","male");
+            //profile_detail.put("date_of_birth","");
+            profile_detail.put("telephone","");
+            profile_detail.put("address","");
+            profile_detail.put("wing_account_number",etWingNumber.getText().toString());
+            profile_detail.put("wing_account_name",etWingName.getText().toString());
+            profile_detail.put("job",etJob.getText().toString());
+            profile.put("profile",profile_detail);
+
+            user.put("username",etUsername.getText().toString());
+            user.put("password",pass);
+            user.put("profile",profile_detail);
+            RequestBody body=RequestBody.create(ConsumeAPI.MEDIA_TYPE,user.toString());
+            Request request=new Request.Builder()
+                    .url(URL_ENDPOINT)
+                    .put(body)
+                    .header("Content-Type","application/json")
+                    .header("Accept","application/json")
+                    .header("Authorization","Basic "+Encode)
+                    .build();
+            Log.e(TAG,URL_ENDPOINT+" "+user);
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    String result=e.getMessage().toString();
+                    Log.d(TAG,"Fail:"+result);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String result=response.body().string();
+                    Log.d(TAG,result);
+                }
+            });
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+    private void PutData(String url,String encode) {
+        /*
+        String t = tvType.getText().toString();
+        username  = etUsername.getText().toString();
+        gender    = tvGender.getText().toString().toLowerCase();
+        dob       = etDob.getText().toString();
+        pob       = tvPob.getText().toString();
+        married   = tvMarried.getText().toString().toLowerCase();
+        phone     = etPhone.getText().toString();
+        location  = tvLocation.getText().toString();
+        job       = etJob.getText().toString();
+        shop_name        = etShopName.getText().toString();
+        shop_addr        = etShopAddr.getText().toString();
+        responsible_name = etResponsible.getText().toString();
+
+        if (t.equals("Public User") || t.equals("Dealer")){
+            wingnumber = etWingNumber.getText().toString();
+            wingname   = etWingName.getText().toString();
+        }else if (t.equals("121 Dealer")){
+            wingnumber = etWingNumber_121.getText().toString();
+            wingname   = etWingName_121.getText().toString();
+        }
+        */
+
+        MediaType media = MediaType.parse("application/json");
+        OkHttpClient client = new OkHttpClient();
+        JSONObject data = new JSONObject();
+        JSONObject pro  = new JSONObject();
+
+        try{
+            data.put("username",name);
+            data.put("password",pass);
+            data.put("first_name",etUsername.getText().toString());
+
+            //pro.put("gender",gender);
+            /*
+            pro.put("data_of_birth",dob);
+            pro.put("address",id_provinces); /////////////
+            pro.put("shop_name",shop_name);
+            pro.put("responsible_officer",responsible_name);
+            pro.put("job",etJob.getText().toString());
+            pro.put("province",id_provinces); /////////////
+            pro.put("marital_status",married);
+            pro.put("shop_address",shop_addr);
+            */
+            pro.put("wing_account_number",etWingNumber.getText().toString());
+            pro.put("wing_account_name",etWingName.getText().toString());
+
+            //pro.put("place_of_birth",id_provinces);
+            data.put("profile",pro);
+            data.put("groups", new JSONArray("[\"1\"]"));
+            //data.put("groups",new JSONArray("["+id_type+"]"));
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        String auth = "Basic " + encode;
+        RequestBody body = RequestBody.create(media, data.toString());
+        Request request = new Request.Builder()
+                .url(url)
+                .put(body)
+                .header("Accept","application/json")
+                .header("Content-Type","application/json")
+                .header("Authorization",auth)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String message = e.getMessage().toString();
+                Log.d("failure Response",message);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String message = response.body().string();
+                Log.d("Response",message);
+
+                //finish();
+            }
+        });
+    }
 }
