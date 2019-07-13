6a0ddba1553def39ceaabc323be9b43e5112374f
Samang Va <49012919+SamangVa@users.noreply.github.com>
2019-07-13 17:07:21 +0700

    correct post

--- app/src/main/java/com/example/lucky_app/Activity/Home.kt
+++ app/src/main/java/com/example/lucky_app/Activity/Home.kt
@@ -437,7 +437,7 @@ class Home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
                                     .url(URL_ENDPOINT1)
                                     .header("Accept","application/json")
                                     .header("Content-Type","application/json")
-                                    .header("Authorization",auth)
+                       //             .header("Authorization",auth)
                                     .build()
                             client1.newCall(request1).enqueue(object : Callback{
                                 override fun onFailure(call: Call, e: IOException) {
